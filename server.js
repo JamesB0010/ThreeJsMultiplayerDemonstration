936cb62de761afa33021c8f4ebe4489208ea8cc7
James Bland <98528984+JamesB0010@users.noreply.github.com>
2024-11-30 02:15:46 +0000

    Now able to tell when a user connects

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 ﻿const express = require('express');
-
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(server);
 const PORT = 3000;
 
 app.use(express.static("./static"));
 
-app.listen(PORT, err =>{
+
+io.on('connection', socket =>{
+    console.log("A user connected");
+})
+
+server.listen(PORT, err =>{
     if(err){
         console.log("Error occured server not starting");
         return;
