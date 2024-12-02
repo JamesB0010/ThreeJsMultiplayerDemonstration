d831dddc5962e4c556d7190532f3fa2326e396e8
James Bland <98528984+JamesB0010@users.noreply.github.com>
2024-12-02 01:41:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server);
+const PORT = 3000;
 
 app.use(express.static("./static"));
 app.use(express.static("./static/CSS"));
